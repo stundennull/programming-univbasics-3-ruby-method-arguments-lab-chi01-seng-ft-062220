5b751d7e8ec0358fbd385682853cec595ce44292
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-10 03:02:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,6 +10,8 @@ def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi my name is #{name} and I am learning to program in #{language}."
 end
 
+introduction_with_language_optional("Edwin")
+
 introduction_with_language_optional("Edwin", "Python")
 
 
